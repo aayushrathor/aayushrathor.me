d1219e89f10f98a6b36d8dcfdc3996a32b2b0c31
aayushrathor <rathoreaayush84@gmail.com>
2022-08-18 14:02:35 +0530

    portfolio website [WIP]

---
+++ lib/seo.ts
@@ -0,0 +1,40 @@
+import { NextSeo } from 'next-seo';
+import { useRouter } from 'next/router';
+
+import type { ComponentProps } from 'react';
+
+export function useSeoProps(
+	props: Partial<ComponentProps<typeof NextSeo>> = {},
+): Partial<ComponentProps<typeof NextSeo>> {
+	const router = useRouter();
+
+	const title = 'aayush ─ developer';
+	const description = "Hey 👋 I'm aayush, DevOps Learner & Linux enthusiast";
+
+	return {
+		title,
+		description,
+		canonical: `https://aayushrathor.me/${router.asPath}`,
+		openGraph: {
+			title,
+			description,
+			site_name: 'aayushrathor',
+			url: `https://aayushrathor.me/${router.asPath}`,
+			type: 'website',
+			images: [
+				{
+					url: '/banner.png',
+					alt: description,
+					width: 1280,
+					height: 720,
+				},
+			],
+		},
+		twitter: {
+			cardType: 'summary_large_image',
+			handle: '@raayush871',
+			site: '@raayush871',
+		},
+		...props,
+	};
+}
